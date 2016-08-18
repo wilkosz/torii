ad61dcb5e7e7180b4a5d9e37812e89b3993f52b3
yethon <ursula_yethon@hotmail.com>
2016-08-18 17:26:00 -0400

    Genericize torii session name.

--- test-support/helpers/torii.js
+++ test-support/helpers/torii.js
@@ -1,8 +1,16 @@
-export function stubValidSession(application,sessionData) {
-  var session = application.__container__.lookup('service:session');
-  var sm = session.get('stateMachine');
-  Ember.run(function() {
+import config from '../../config/environment';
+
+const {
+  torii: { sessionServiceName }
+} = config;
+
+export function stubValidSession(application, sessionData) {
+  let session = application.__container__.lookup(`service:${sessionServiceName}`);
+
+  let sm = session.get('stateMachine');
+  Ember.run(() => {
     sm.send('startOpen');
     sm.send('finishOpen', sessionData);
   });
 }
+
