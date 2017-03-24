3cbd055d3adae3f51ec54d614b4660fccb0624d5
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2017-03-24 15:35:04 +0100

    test with 1.13.13 instead of 1.13.16
    …as bower cannot find 1.13.16

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,9 +8,9 @@ module.exports = {
       }
     },
     {
-      name: 'Ember 1.13.16',
+      name: 'Ember 1.13.13',
       dependencies: {
-        'ember': '1.13.16'
+        'ember': '1.13.13'
       }
     },
     {
