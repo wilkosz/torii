wilkosz/torii

commit f0b214f069f59c1ff037898267bf2c4c9853f316

Deprecate Redirect Handler, use public/redirect.html as redirect target
 * Warn in opener window when app is target of OAuth redirect
 * add directions to readme
 * Update default redirectUri
 * Add Security section to readme

addon/providers/oauth2-code.js
@@ -7,10 +7,14 @@ import randomUrlSafe from 'torii/lib/random-url-safe';
 var computed = Ember.computed;
 
 function currentUrl(){
-  return [window.location.protocol,
-          "//",
-          window.location.host,
-          window.location.pathname].join('');
+  var url = [window.location.protocol,
+             "//",
+             window.location.host,
+             window.location.pathname].join('');
+  if (url.substr(-1) !== '/') {
+    url += '/';
+  }
+  return url;
 }
 
 /**
@@ -102,7 +106,7 @@ var Oauth2 = Provider.extend({
   responseParams: requiredProperty(),
 
   redirectUri: computed(function defaultRedirectUri(){
-    return currentUrl();
+    return `${currentUrl()}torii/redirect.html`;
   }),
 
   buildQueryString: function(){

addon/lib/assert.js
@@ -1,0 +1,5 @@
+export default function assert(message, test) {
+  if (!test) {
+    console.error(message); // jshint ignore:line
+  }
+}

addon/mixins/ui-service-mixin.js
@@ -1,7 +1,10 @@
 import UUIDGenerator from 'torii/lib/uuid-generator';
 import PopupIdSerializer from 'torii/lib/popup-id-serializer';
 import ParseQueryString from 'torii/lib/parse-query-string';
-export var CURRENT_REQUEST_KEY = '__torii_request';
+import assert from 'torii/lib/assert';
+export const CURRENT_REQUEST_KEY = '__torii_request';
+export const WARNING_KEY = '__torii_redirect_warning';
+import { getConfiguration } from 'torii/configuration';
 
 var on = Ember.on;
 
@@ -19,11 +22,11 @@ var ServicesMixin = Ember.Mixin.create({
   },
 
   // Open a remote window. Returns a promise that resolves or rejects
-  // accoring to if the iframe is redirected with arguments in the URL.
+  // according to whether the window is redirected with arguments in the URL.
   //
   // For example, an OAuth2 request:
   //
-  // iframe.open('http://some-oauth.com', ['code']).then(function(data){
+  // popup.open('http://some-oauth.com', ['code']).then(function(data){
   //   // resolves with data.code, as from http://app.com?code=13124
   // });
   //
@@ -42,7 +45,7 @@ var ServicesMixin = Ember.Mixin.create({
 
       var pendingRequestKey = PopupIdSerializer.serialize(remoteId);
       localStorage.setItem(CURRENT_REQUEST_KEY, pendingRequestKey);
-
+      localStorage.removeItem(WARNING_KEY);
 
       service.openRemote(url, pendingRequestKey, options);
       service.schedulePolling();
@@ -64,6 +67,16 @@ var ServicesMixin = Ember.Mixin.create({
       }
 
       service.one('didClose', function(){
+        let hasWarning = localStorage.getItem(WARNING_KEY);
+        if (hasWarning) {
+          localStorage.removeItem(WARNING_KEY);
+          let configuration = getConfiguration();
+
+          assert(`[Torii] Using an OAuth redirect that loads your Ember App is deprecated and will be
+              removed in a future release, as doing so is a potential security vulnerability.
+              Hide this message by setting \`allowUnsafeRedirect: true\` in your Torii configuration.
+          `, configuration.allowUnsafeRedirect);
+        }
         var pendingRequestKey = localStorage.getItem(CURRENT_REQUEST_KEY);
         if (pendingRequestKey) {
           localStorage.removeItem(pendingRequestKey);
@@ -72,7 +85,7 @@ var ServicesMixin = Ember.Mixin.create({
         // If we don't receive a message before the timeout, we fail. Normally,
         // the message will be received and the window will close immediately.
         service.timeout = Ember.run.later(service, function() {
-          reject(new Error("remote was closed, authorization was denied, or a authentication message otherwise not received before the window closed."));
+          reject(new Error("remote was closed, authorization was denied, or an authentication message otherwise not received before the window closed."));
         }, 100);
       });
 
@@ -88,8 +101,6 @@ var ServicesMixin = Ember.Mixin.create({
           });
         }
       });
-
-
     }).finally(function(){
       // didClose will reject this same promise, but it has already resolved.
       service.close();
@@ -111,7 +122,6 @@ var ServicesMixin = Ember.Mixin.create({
     this.clearTimeout();
   },
 
-
   schedulePolling: function(){
     this.polling = Ember.run.later(this, function(){
       this.pollRemote();
@@ -128,9 +138,6 @@ var ServicesMixin = Ember.Mixin.create({
   stopPolling: on('didClose', function(){
     Ember.run.cancel(this.polling);
   })
-
-
-
 });
 
 export default ServicesMixin;

addon/redirect-handler.js
@@ -7,7 +7,7 @@
  */
 
 import PopupIdSerializer from "./lib/popup-id-serializer";
-import { CURRENT_REQUEST_KEY } from "./mixins/ui-service-mixin";
+import { CURRENT_REQUEST_KEY, WARNING_KEY } from "./mixins/ui-service-mixin";
 import configuration from 'torii/configuration';
 
 var RedirectHandler = Ember.Object.extend({
@@ -20,6 +20,7 @@ var RedirectHandler = Ember.Object.extend({
       windowObject.localStorage.removeItem(CURRENT_REQUEST_KEY);
       if (pendingRequestKey) {
         var url = windowObject.location.toString();
+        windowObject.localStorage.setItem(WARNING_KEY, 'true');
         windowObject.localStorage.setItem(pendingRequestKey, url);
 
         var remoteServiceName = configuration.remoteServiceName || 'popup';

public/redirect.html
@@ -1,0 +1,17 @@
+<html>
+  <head>
+    <title>Torii OAuth Redirect</title>
+    <script>
+      var CURRENT_REQUEST_KEY = '__torii_request';
+      var pendingRequestKey = window.localStorage.getItem(CURRENT_REQUEST_KEY);
+
+      if (pendingRequestKey) {
+        window.localStorage.removeItem(CURRENT_REQUEST_KEY);
+        var url = window.location.toString();
+        window.localStorage.setItem(pendingRequestKey, url);
+      }
+
+      window.close();
+    </script>
+  </head>
+</html>

commit b47bbdf0a21342592f94e483daa2f9762bd45ede

Make redirectUri configurable again
This is a regression introduced by commit f0b214f ("Deprecate Redirect
Handler, use public/redirect.html as redirect target", 2017-05-22).

addon/providers/oauth2-code.js
@@ -105,7 +105,7 @@ var Oauth2 = Provider.extend({
   */
   responseParams: requiredProperty(),
 
-  redirectUri: computed(function defaultRedirectUri(){
+  redirectUri: configurable('redirectUri', function (){
     return `${currentUrl()}torii/redirect.html`;
   }),
 